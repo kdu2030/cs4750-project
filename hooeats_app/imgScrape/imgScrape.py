686129c06f5d3c92b9db37e56d1b9114e49a107d
Ji <chen.jikai44@gmail.com>
2023-02-06 22:19:21 -0500

    Update imgScrape.py
    Grabs the first image URL from a given recipe_title. Will update once the dataset is complete and uploaded.

--- hooeats_app/imgScrape/imgScrape.py
+++ hooeats_app/imgScrape/imgScrape.py
@@ -3,28 +3,23 @@ from bs4 import BeautifulSoup
 
 def recipe_images(recipe_title):
 
-    print("1")
     recipe_title = recipe_title.replace(" ", "-")
-    url = f"https://www.foods.com/recipe/{recipe_title}"
-    print("2")
+    url = f"https://www.food.com/recipe/{recipe_title}"
     page = requests.get(url)
-    print("3")
     parser = BeautifulSoup(page.content, "html.parser")
 
-    print("4")
     #find div with class "primary-image"
-    div = parser.find("div", class_="primary-image")
-
-    print("5")
-    #image
-    image = div.get("srcset").split(",")[0].strip().split(" ")[0]
-
-    print("6")
-    return image
+    div = str(parser.find("div", class_="primary-image"))
+    
+    #I will change the naming of each variables later on
+    begin = div.find("srcset") + 8
+    first = div[begin:]
+    end = first.find(".jpg") + 4
+    final = first[:end]
+    return final
 
 def main():
-    print("0")
-    recipe_images("15 bean soup 51803")
+    print(recipe_images("best banana bread 2886"))
 
 if __name__ == "__main__":
     main()
