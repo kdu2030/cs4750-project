c7a2fb7d60e45f1880ffb174400e28d680878db7
kdu2030 <kdu2030@gmail.com>
2023-02-12 21:55:17 -0500

    Add Nav Bar and Home Page

--- hooeats_app/views.py
+++ hooeats_app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
+
 
 # Create your views here.
+
+def index(request: HttpRequest) -> HttpResponse:
+    return render(request, "hooeats_app/index.html")
