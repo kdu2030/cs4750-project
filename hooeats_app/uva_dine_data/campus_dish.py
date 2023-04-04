3d26fae82c2dad68d656789239fe4e1456e430a7
kdu2030 <kdu2030@gmail.com>
2023-04-04 18:20:49 -0400

    Fix Signup and Make Data Processing Modular

--- hooeats_app/uva_dine_data/campus_dish.py
+++ hooeats_app/uva_dine_data/campus_dish.py
@@ -10,6 +10,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from datetime import datetime, timedelta
 from selenium.webdriver.common.by import By
 from database import HooEatsDatabase
+from populator import insert_into_db
 
 class CampusDishParser:
 
@@ -198,12 +199,7 @@ class CampusDishParser:
                     except:
                         continue
         
-        while True:
-            try:
-                self.insert_into_db(meals)
-                break
-            except:
-                continue
+        insert_into_db(meals, self.dining_hall)
         return meals
 
     def change_meal(self, meal_title: str):
@@ -259,26 +255,7 @@ class CampusDishParser:
             if type(meal[key]) == str and meal[key].find("'") != -1:
                 meal[key] = meal[key].replace("'", "''");
         return meal
-    
 
-    def insert_into_db(self, meal_data: List[Dict[str, Dict]]):
-        date_strs = self.get_week_dates()
-        database = HooEatsDatabase()
-        for meal in meal_data:
-            meal = self.esc_apostrophes(meal)
-
-            meal_date = date_strs[meal["day_of_week"]]
-
-            uva_description_select = "SELECT * FROM uva_descriptions WHERE title='{title}' AND section='{section}' AND dining_hall='{dining_hall}';".format(title=meal["title"], section=meal["section"], dining_hall=self.dining_hall);
-            select_results = database.execute(uva_description_select, expect_results=True)
-            if len(select_results) == 0:
-                uva_description_insert = "INSERT INTO uva_descriptions (title, dining_hall, section, serving_size, calories, calories_from_fat, total_fat, saturated_fat, sugar, protein, dietary_fiber, total_carbohydrates, sodium) VALUES ('{title}', '{dining_hall}', '{section}', '{serving_size}', {calories:.2f}, {calories_from_fat:.2f}, {total_fat:.2f}, {saturated_fat:.2f}, {sugar:.2f}, {protein:.2f}, {dietary_fiber:.2f}, {total_carbohydrates:.2f}, {sodium:.2f})".format(title=meal["title"], dining_hall=self.dining_hall, section=meal["section"], serving_size=meal["serving_size"], calories=meal["calories"], calories_from_fat=meal["calories_from_fat"], total_fat=meal["total_fat"], saturated_fat=meal["saturated_fat"], sugar=meal["sugar"], protein=meal["protein"], dietary_fiber=meal["dietary_fiber"], total_carbohydrates=meal["total_carbohydrates"], sodium=meal["sodium"])
-                database.execute(uva_description_insert, expect_results=False)
-
-
-            uva_meals_query = "INSERT INTO uva_meals (title, dining_hall, section, meal_type, meal_date) VALUES ('{title}', '{dining_hall}', '{section}', '{meal_type}', '{meal_date}')".format(title=meal["title"], dining_hall=self.dining_hall, section=meal["section"], meal_type=meal["type"], meal_date=meal_date)
-            database.execute(uva_meals_query, expect_results=False)
-        database.close()
 
 
 def main():
